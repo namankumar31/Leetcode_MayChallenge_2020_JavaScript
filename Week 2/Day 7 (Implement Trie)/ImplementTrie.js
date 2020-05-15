d8ab7815ef2910285a5f2b8ae8f866b34bade7a6
namankumar31 <naman_89@yahoo.com>
2020-05-14 21:05:26 -0400

    Create ImplementTrie.js

---
+++ Week 2/Day 7 (Implement Trie)/ImplementTrie.js
@@ -0,0 +1,59 @@
+/**
+ * Initialize your data structure here.
+ */
+var Trie = function() {
+    this.root = {};
+};
+
+/**
+ * Inserts a word into the trie. 
+ * @param {string} word
+ * @return {void}
+ */
+Trie.prototype.insert = function(word) {
+    let node = this.root;
+    for(let char of word){
+        if(node[char] == null)
+            node[char] = {};
+        node = node[char];
+    }
+    node.isEnd = true;
+};
+
+/**
+ * Returns if the word is in the trie. 
+ * @param {string} word
+ * @return {boolean}
+ */
+Trie.prototype.search = function(word) {
+    let node = this.root;
+    for(let char of word){
+        node = node[char];
+        if(node == null)
+            return false;
+    }
+    return node != null && node.isEnd === true;
+};
+
+/**
+ * Returns if there is any word in the trie that starts with the given prefix. 
+ * @param {string} prefix
+ * @return {boolean}
+ */
+Trie.prototype.startsWith = function(prefix) {
+    let node = this.root;
+    for(let char of prefix){
+        node = node[char];
+        if(node == null)
+            return false;
+    }
+    return node != null;
+};
+
+/** 
+ * Your Trie object will be instantiated and called as such:
+ * var obj = new Trie()
+ * obj.insert(word)
+ * var param_2 = obj.search(word)
+ * var param_3 = obj.startsWith(prefix)
+ */

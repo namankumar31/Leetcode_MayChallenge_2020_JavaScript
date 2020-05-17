6c13a018b6a91e49023c6e38b45c605467c97d56
namankumar31 <naman_89@yahoo.com>
2020-05-17 12:09:12 -0400

    Create OddEvenLinkedList.js

---
+++ Week 3/Day 2 (Odd Even Linked List)/OddEvenLinkedList.js
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var oddEvenList = function(head) {
+    if(head == null){
+        return head;
+    }
+    
+    let odd = head;
+    let even = head.next;
+    let dummyEven = head.next;
+    
+    while(even != null && even.next != null){
+        odd.next = even.next;
+        even.next = even.next.next;
+        odd = odd.next;
+        even = even.next;
+    }
+    
+    odd.next = dummyEven;
+    
+    return head;
+};

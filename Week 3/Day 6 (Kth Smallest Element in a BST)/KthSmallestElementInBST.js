13cad4fdc95cfc6327d56f89e5110b183c50c294
namankumar31 <naman_89@yahoo.com>
2020-05-20 21:51:48 -0400

    Update KthSmallestElementInBST.js

--- Week 3/Day 6 (Kth Smallest Element in a BST)/KthSmallestElementInBST.js
+++ Week 3/Day 6 (Kth Smallest Element in a BST)/KthSmallestElementInBST.js
@@ -1,1 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} k
+ * @return {number}
+ */
+var kthSmallest = function(root, k) {
+    let arr = [];
+    inOrder(root,arr);
+    return arr[k-1];
+};
 
+function inOrder(root, arr){
+    if(root === null)
+        return [];
+    
+    if(root.left)
+        inOrder(root.left, arr);
+    
+    arr.push(root.val);
+    
+    if(root.right)
+        inOrder(root.right, arr);
+}
